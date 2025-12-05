65cb48709fdb0b0d9d6cfdf43ef1ce87f517f029
Amina <amina.ali.31032008@gmail.com>
2025-12-05 21:20:13 +0500

    Improve error handling for weather API requests
    Handle connection errors when fetching weather data.

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 import requests
 
 API_KEY="c555d51a2de0407ba27150017250512"
@@ -9,31 +8,30 @@ st.write("Welcome to my weather dashboard application!")
 
 def get_weather(city):
     url = f"http://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={city}&days=1&aqi=no&alerts=no"
-    response = requests.get(url)
-    return response.json()
+    try:
+        response = requests.get(url)
+        return response.json()
+    except requests.exceptions.ConnectionError:
+         return None
 
 city = st.text_input("Enter city name:", "Cairo")
 if st.button("Submit"):
-    try:
-        data=get_weather(city)
-    except ConnectionError:
+    data=get_weather(city)
+    if data is None:
         st.write("Error fetching data. Please check your connection.")
-    if 'current' in data:
-        main = data['current']['condition']['text']
-        temp = data["current"]["temp_c"]
-        feels = data["current"]["feelslike_c"]
-        humidity = data["current"]["humidity"]
-        wind = data["current"]["wind_mph"]
-        visibility = data['current']['vis_km']
-        icon_url = "https:"+data["current"]["condition"]["icon"]
-        st.image(icon_url)
-        st.write('Weather Condition:', main)
-        st.divider(width='stretch')
-        col1,col2,col3, col4= st.columns(4)  
-        col1.metric(label="Temperature (°C)", value=f"{temp} °C", delta=f"Feels like {feels} °C",border=True)
-        col2.metric(label="Humidity (%)", value=f"{humidity} %",border=True)
-        col3.metric(label="Wind Speed (m/s)", value=f"{wind} m/s",border=True)
-        col4.metric(label="Visibility (km)", value=f"{visibility} km",border=True)
-
-    else: 
-        st.write("City not found. Please try again.")
+    else:
+            main = data['current']['condition']['text']
+            temp = data["current"]["temp_c"]
+            feels = data["current"]["feelslike_c"]
+            humidity = data["current"]["humidity"]
+            wind = data["current"]["wind_mph"]
+            visibility = data['current']['vis_km']
+            icon_url = "https:"+data["current"]["condition"]["icon"]
+            st.image(icon_url)
+            st.write('Weather Condition:', main)
+            st.divider(width='stretch')
+            col1,col2,col3, col4= st.columns(4)  
+            col1.metric(label="Temperature (°C)", value=f"{temp} °C", delta=f"Feels like {feels} °C",border=True)
+            col2.metric(label="Humidity (%)", value=f"{humidity} %",border=True)
+            col3.metric(label="Wind Speed (m/s)", value=f"{wind} m/s",border=True)
+            col4.metric(label="Visibility (km)", value=f"{visibility} km",border=True)
